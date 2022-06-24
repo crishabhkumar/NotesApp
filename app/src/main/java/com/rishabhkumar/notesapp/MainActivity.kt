e719d92bf8baede48f179a19bf31ec67651b7181
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-24 23:01:40 +0530

    Navigation setup done.

--- app/src/main/java/com/rishabhkumar/notesapp/MainActivity.kt
+++ app/src/main/java/com/rishabhkumar/notesapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rishabhkumar.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragmentContainerView)
+        setupActionBarWithNavController(navController)
+    }
+
+    //Navigation Setup function
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
