4121ba8608d2e1438830e0aec55ca7bee767b650
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-24 16:33:31 +0530

    SplashScreen done.

---
+++ app/src/main/java/com/rishabhkumar/notesapp/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.rishabhkumar.notesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+            }, 2000)
+    }
+}
