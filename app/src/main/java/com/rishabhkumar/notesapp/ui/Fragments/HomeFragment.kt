f8a774255589ce9e73b8e8afb2da5bb099e6558f
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-24 23:57:19 +0530

    Functionality of add button is done.

--- app/src/main/java/com/rishabhkumar/notesapp/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/rishabhkumar/notesapp/ui/Fragments/HomeFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.rishabhkumar.notesapp.R
+import com.rishabhkumar.notesapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding.btnAddNotes.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_homeFragment_to_createNotesFragment)
+            //this id = action_homeFragment_to_createNotesFragment comes from nav graph
+        }
+        return binding.root
     }
 
 }
