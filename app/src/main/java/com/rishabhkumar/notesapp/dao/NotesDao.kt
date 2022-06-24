86a1744c01f88fa3714e1e7ec04bd09493e12c18
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-25 01:13:54 +0530

    Entity and DAO creation is done.

---
+++ app/src/main/java/com/rishabhkumar/notesapp/dao/NotesDao.kt
@@ -0,0 +1,22 @@
+package com.rishabhkumar.notesapp.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.rishabhkumar.notesapp.model.Notes
+
+//Data Access object
+@Dao
+interface NotesDao {
+
+    @Query("SELECT * FROM Notes")
+    fun getNotes(): LiveData<List<Notes>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNotes(note:Notes)
+
+    @Query("DELETE FROM Notes WHERE id = id")
+    fun deleteNotes(id:Int)
+
+    @Update
+    fun updateNotes(note:Notes)
+}
---
+++ app/src/main/java/com/rishabhkumar/notesapp/model/Notes.kt
@@ -0,0 +1,19 @@
+package com.rishabhkumar.notesapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+//Entity class
+
+@Entity(tableName = "Notes")
+class Notes (
+    @PrimaryKey(autoGenerate = true)
+    var id:Int?=null,
+    var title:String,
+    var subtitle:String,
+    var notes:String,
+    var date:String,
+    var priority:String
+
+)
