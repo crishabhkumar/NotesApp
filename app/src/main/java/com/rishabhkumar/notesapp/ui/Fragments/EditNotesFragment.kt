8766a5c8b04dff6d0312e7b348c34a0616037e4c
Rishabh Kumar <crishabhkumar@gmail.com>
2022-06-26 02:24:36 +0530

    Notes deletion code is done.

--- app/src/main/java/com/rishabhkumar/notesapp/ui/Fragments/EditNotesFragment.kt
+++ app/src/main/java/com/rishabhkumar/notesapp/ui/Fragments/EditNotesFragment.kt
@@ -1,14 +1,15 @@
 package com.rishabhkumar.notesapp.ui.Fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.rishabhkumar.notesapp.R
 import com.rishabhkumar.notesapp.databinding.FragmentEditNotesBinding
 import com.rishabhkumar.notesapp.model.Notes
@@ -21,7 +22,7 @@ class EditNotesFragment : Fragment() {
     val oldNotes by navArgs<EditNotesFragmentArgs>()
     lateinit var binding: FragmentEditNotesBinding
     var priority: String = "1"
-    val viewModel : NotesViewModel by viewModels()
+    val viewModel: NotesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +30,7 @@ class EditNotesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentEditNotesBinding.inflate(layoutInflater, container, false)
-
+        setHasOptionsMenu(true)
         getData()
         priorityChange()
 
@@ -59,12 +60,13 @@ class EditNotesFragment : Fragment() {
         )
         viewModel.updateNotes(data)
 
-        Toast.makeText(requireContext(),"Note updated.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(), "Note updated.", Toast.LENGTH_SHORT).show()
         //finishing the fragment after creation of note
         Navigation.findNavController(it!!).navigate(R.id.action_editNotesFragment_to_homeFragment)
 
     }
-    private fun getCurrentDate() : String{
+
+    private fun getCurrentDate(): String {
         val c = Calendar.getInstance().time
         println("Current time => $c")
         val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
@@ -74,25 +76,25 @@ class EditNotesFragment : Fragment() {
     }
 
     //function for retrieving the data from home fragment
-    private fun getData(){
+    private fun getData() {
         binding.edtEditTitle.setText(oldNotes.data.title)
         binding.edtEditSubTitle.setText(oldNotes.data.subtitle)
         binding.edtEditNotes.setText(oldNotes.data.notes)
 
-        when(oldNotes.data.priority){
-            "1"->{
+        when (oldNotes.data.priority) {
+            "1" -> {
                 priority = "1"
                 binding.pGreenEdit.setImageResource(R.drawable.done_icon)
                 binding.pRedEdit.setImageResource(0)
                 binding.pYellowEdit.setImageResource(0)
             }
-            "2"->{
+            "2" -> {
                 priority = "2"
                 binding.pRedEdit.setImageResource(R.drawable.done_icon)
                 binding.pGreenEdit.setImageResource(0)
                 binding.pYellowEdit.setImageResource(0)
             }
-            "3"->{
+            "3" -> {
                 priority = "3"
                 binding.pYellowEdit.setImageResource(R.drawable.done_icon)
                 binding.pGreenEdit.setImageResource(0)
@@ -101,7 +103,7 @@ class EditNotesFragment : Fragment() {
         }
     }
 
-    private fun priorityChange(){
+    private fun priorityChange() {
         //when someone wants to change priority of task or notes
         binding.pGreenEdit.setOnClickListener {
             priority = "1"
@@ -121,5 +123,34 @@ class EditNotesFragment : Fragment() {
             binding.pGreenEdit.setImageResource(0)
             binding.pRedEdit.setImageResource(0)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.iconMenuDelete) {
+            val bottomSheet: BottomSheetDialog =
+                BottomSheetDialog(requireContext(), R.style.BottomSheetStyle)
+            bottomSheet.setContentView(R.layout.dialog_delete)
+            bottomSheet.show()
+
+            val txtViewNo = bottomSheet.findViewById<TextView>(R.id.txtNo)
+            val txtViewYes = bottomSheet.findViewById<TextView>(R.id.txtYes)
+
+            txtViewYes?.setOnClickListener {
+                viewModel.deleteNotes(oldNotes.data.id!!)
+                Navigation.findNavController(it!!)
+                    .navigate(R.id.action_editNotesFragment_to_homeFragment)
+            }
+
+            txtViewNo?.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
